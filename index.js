3caaedaf60cc0cccca94e1d25a774505b814ef43
LeVernicus <zackbaum90@gmail.com>
2022-10-12 18:28:22 -0600

    working on adding maps filter

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Require the necessary discord.js classes
 const { Client, GatewayIntentBits,  Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { token, clientId, guildId } = require('dotenv').config();
-const rest = new REST({ version: '10' }).setToken(token);
+require('dotenv').config();
+const rest = new REST({ version: '10' }).setToken(process.env.token);
 const maps = require('./maps.json');
-console.log('token');
 // Create a new client instance
 const client = new Client({
 	intents: [
@@ -17,6 +16,31 @@ const client = new Client({
 function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
+function mapFilter(mode, maps) {
+	let mapsFiltered = {
+		"mapName": "",
+        "internalName": "",
+        "firstAdded": "",
+        "lastUpdated": "",
+        "competitive": false,
+        "wingman": false,
+        "casual": false,
+        "deathmatch": false,
+        "armsRace": false,
+        "flyingScoutsman": false,
+        "retakes": false,
+        "author": "",
+        "comments": ""
+	};
+	for (let i=0; i<maps.length; i++) {
+		let j = 0;
+		if (maps[i].mode === true) {
+			mapsFiltered[j] = maps[i];
+			j++
+		}
+	}
+	return mapsFiltered;
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => console.log(`${client.user.tag} is ready!`));
@@ -27,24 +51,40 @@ client.on('interactionCreate', (interaction) => {
 		interaction.reply({ content: interaction.member.nickname + ' rolled the number ' + roll.toString() + '.' + ' (' + interaction.options.getInteger('minval') + '-' + interaction.options.getInteger('maxval') + ')' });
 	}
 	if (interaction.commandName === 'randommap') {
-		const roll = randomInt(0, maps.length);
-		interaction.reply({ content: maps[roll].internalName });
+		mapsFiltered = mapFilter('competitive', maps);
+		console.log(mapsFiltered)
+		// if (interaction.options["_hoistedOptions"][0] !== undefined) {
+		// 	console.log("Mode works.");
+		// 	if ()
+		// }
+		// const roll = randomInt(0, maps.length)
+		// if (interaction.options["_hoistedOptions"][0] === null) {
+		// 	console.log('Options is null!');
+		// }
+		// if (interaction.options["_hoistedOptions"][0] === undefined) {
+		// 	console.log('Options is undefined!');
+		// }
+		// console.log(interaction.options["_hoistedOptions"][0].value);
+		// console.log(roll.toString())
+		// console.log(typeof roll)
+		// console.log(maps[0].internalName);
+		// interaction.reply({ content: maps[roll].internalName });
 	}
 });
-/* 	else {
-		let maps_filter = [null];
-		for (let i = 0; i < maps.length; i++) {
-			if (maps[i].competitive) {
-				console.log(maps[i]);
-				console.log(maps[i].internalName);
-				console.log(maps[i].competitive);
-				maps_filter += maps[i];
-			}
-		}
-		const roll = randomInt(0, maps_filter.length);
-		interaction.reply({ content: maps_filter[roll].internalName });
-	} */
-
+ 	// else {
+	// 	let maps_filter = [null];
+	// 	for (let i = 0; i < maps.length; i++) {
+	// 		if (maps[i].competitive) {
+	// 			console.log(maps[i]);
+	// 			console.log(maps[i].internalName);
+	// 			console.log(maps[i].competitive);
+	// 			maps_filter += maps[i];
+	// 		}
+	// 	}
+	// 	const roll = randomInt(0, maps_filter.length);
+	// 	interaction.reply({ content: maps_filter[roll].internalName });
+	// }
+	
 async function main() {
 	const commands = [
 		{
@@ -110,7 +150,7 @@ async function main() {
 	];
 	try {
 		console.log('Started refreshing application (/) commands.');
-		await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+		await rest.put(Routes.applicationGuildCommands(process.env.clientId, process.env.guildId), {
 			body: commands,
 		});
 		// client.login(token)
@@ -124,4 +164,4 @@ main();
 
 // Login to Discord with your client's token
 
-client.login(token);
+client.login(process.env.token);
